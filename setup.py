dbd921304016215d3055133fd2491cf9a02dfb80
Callum J Hays <callumjhays@gmail.com>
2022-09-19 15:58:57 +1000

    fix: pypi classifiers again

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        "Development Status :: 2 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         # Indicate who your project is intended for
-        "Intended Audience :: Developers,",
+        "Intended Audience :: Developers",
         # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
