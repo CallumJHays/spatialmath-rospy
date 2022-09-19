6c0a4ca89297a74c586004645802f3ce990b1d8e
Callum J Hays <callumjhays@gmail.com>
2022-09-19 14:30:28 +1000

    fix: add SE3, SO3 and UnitQ.. to __all__

--- spatialmath_rospy/__init__.py
+++ spatialmath_rospy/__init__.py
@@ -4,4 +4,4 @@ from spatialmath_rospy.convert import to_spatialmath, to_ros
 from spatialmath_rospy.ros_compat_mixin import ROSCompatMixin, SE3, SO3, UnitQuaternion
 from spatialmath_rospy.monkey_patch_spatialmath import monkey_patch_spatialmath
 
-__all__ = ["to_spatialmath", "to_ros", "ROSCompatMixin", "monkey_patch_spatialmath"]
+__all__ = ["to_spatialmath", "to_ros", "ROSCompatMixin", "monkey_patch_spatialmath", "SE3", "SO3", "UnitQuaternion"]
