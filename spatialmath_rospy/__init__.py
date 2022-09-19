1f3527121053a43f6c4eb4785dfd91abfb68ce2b
Callum J Hays <callumjhays@gmail.com>
2022-09-19 15:31:43 +1000

    ci: trigger new release

--- spatialmath_rospy/__init__.py
+++ spatialmath_rospy/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.0.0"
 
 from spatialmath_rospy.convert import to_spatialmath, to_ros
-from spatialmath_rospy.ros_compat_mixin import ROSCompatMixin, SE3, SO3, UnitQuaternion
 from spatialmath_rospy.monkey_patch_spatialmath import monkey_patch_spatialmath
+from spatialmath_rospy.ros_compat_mixin import ROSCompatMixin, SE3, SO3, UnitQuaternion
 
 __all__ = ["to_spatialmath", "to_ros", "ROSCompatMixin", "monkey_patch_spatialmath", "SE3", "SO3", "UnitQuaternion"]
